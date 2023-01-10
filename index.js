5fc8e69a38e4f0fe7d870a716d226ac00029c206
Bianca Aspin <bianca@mj94.net>
2023-01-09 19:16:14 -0800

    Declared and assigned variables to complete lab.

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+
+// Test #1
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+
+//Test #2
+const random = Math.floor(Math.random() * 100) + 1;
+
+// Test #3
+const num3 = 4;
+const num4 = 5;
+const mod = num3 % num4;
+
+// Test #4
+const max = Math.max(5, 10, 15, 20);
